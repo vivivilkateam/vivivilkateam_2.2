76d0a8c39efc0d96452feb375784352145600209
Rokan3583 <joskiytelik@gmail.com>
2025-02-10 19:51:29 +0300

    new work

--- Dark_school/main.py
+++ Dark_school/main.py
@@ -1,4 +1,10 @@
 import person
 import tkinter
 pers = person.Units(100,100,100,2,15)
-print(pers)
+pl1 = person.game_player(100,100,100,2,15,50,'шлем','меч',100)
+en1 = person.game_enemy(200,100,75,3,10,25,34,'кинжалы')
+en2 = person.game_enemy(100,200,150,1,30,75,97,'дубина')
+print(str(pers))
+print(str(pl1))
+print(str(en1))
+print(str(en2))
--- Dark_school/person.py
+++ Dark_school/person.py
@@ -9,3 +9,34 @@ class Units:
     def __str__(self):
         return (f'координаты: x = {self.x}, y = {self.y}, '
                 f'здоровье: {self.hp}, Скорость: {self.speed}, Урон: {self.damage}')
+class game_player(Units):
+    def __init__(self, x, y, hp, speed, damage, shield, armor, weapons, xp):
+        self.x = x
+        self.y = y
+        self.hp = hp
+        self.speed = speed
+        self.damage = damage
+        self.shield = shield
+        self.armor = armor
+        self.weapons = weapons
+        self.xp = xp
+
+    def __str__(self):
+        return (f'координаты: x = {self.x}, y = {self.y}, '
+                    f'здоровье: {self.hp}, Скорость: {self.speed}, Урон: {self.damage},'
+                f' Защита:{self.shield}, Броня:{self.armor}, Оружие:{self.weapons}, Опыт:{self.xp}')
+class game_enemy(Units):
+    def __init__(self, x, y, hp, speed, damage, shield, drope_xp,inventory_loot):
+        self.x = x
+        self.y = y
+        self.hp = hp
+        self.speed = speed
+        self.damage = damage
+        self.shield = shield
+        self.xp = drope_xp
+        self.loot = inventory_loot
+
+    def __str__(self):
+        return (f'координаты: x = {self.x}, y = {self.y}, '
+                    f'здоровье: {self.hp}, Скорость: {self.speed}, Урон: {self.damage},'
+                f' Защита:{self.shield}, Выпадаемый опыт:{self.xp}, Лут:{self.loot}')
